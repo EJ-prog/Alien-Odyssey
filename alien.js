95c9a01c3b362e1da076c585cc2771e77c09bad9
Elisabeth <56486009+EJ-prog@users.noreply.github.com>
2022-01-22 13:33:23 -0800

    updated sprites

--- alien.js
+++ alien.js
@@ -2,21 +2,22 @@ class Alien{
     constructor(game) {
         this.game = game;
         this.spritesheet = ASSET_MANAGER.getAsset("./Sprites_and_Assets/character-sprites-player-alien-R.png");
-        this.animator = new Animator(this.spritesheet, 0, 420, 96, 110, 10, 0.3);
-        this.x =0;
-        this.y=0;
-        this.speed = 120;
+        //this.animator = new Animator(this.spritesheet, 21, 397, 155, 207, 7, 0.3); //run and shoot
+        this.animator = new Animator(this.spritesheet, 21, 397, 155, 207, 7, 0.3);
+        // this.x =0;
+        // this.y=0;
+        // this.speed = 120;
     };
 
     update() {
-        if(this.y < 500) {
-            this.y += this.speed*this.game.clockTick;
-        } else if (this.x < 1024) {
-            this.x += this.speed*this.game.clockTick;
-        } else {
-            this.x = 0;
-            this.y = 0;
-        }
+        // if(this.y < 500) {
+        //     this.y += this.speed*this.game.clockTick;
+        // } else if (this.x < 1024) {
+        //     this.x += this.speed*this.game.clockTick;
+        // } else {
+        //     this.x = 0;
+        //     this.y = 0;
+        // }
     };
 
     draw(ctx) {
