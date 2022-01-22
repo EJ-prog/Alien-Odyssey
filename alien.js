05bd43caa0ab5c428525b60902453582cc60d3da
Elisabeth <56486009+EJ-prog@users.noreply.github.com>
2022-01-22 12:40:19 -0800

    Basic code and started on alien. added mirrored version of assets for alien.

--- alien.js
+++ alien.js
@@ -1,2 +1,25 @@
-//dsjfndslsd
-class alien{}
+class Alien{
+    constructor(game) {
+        this.game = game;
+        this.spritesheet = ASSET_MANAGER.getAsset("./Sprites_and_Assets/character-sprites-player-alien-R.png");
+        this.animator = new Animator(this.spritesheet, 0, 420, 96, 110, 10, 0.3);
+        this.x =0;
+        this.y=0;
+        this.speed = 120;
+    };
+
+    update() {
+        if(this.y < 500) {
+            this.y += this.speed*this.game.clockTick;
+        } else if (this.x < 1024) {
+            this.x += this.speed*this.game.clockTick;
+        } else {
+            this.x = 0;
+            this.y = 0;
+        }
+    };
+
+    draw(ctx) {
+        this.animator.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+    };
+}
--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-class main{}
+const gameEngine = new GameEngine();
+
+const ASSET_MANAGER = new AssetManager();
+
+ASSET_MANAGER.queueDownload("./Sprites_and_Assets/character-sprites-player-alien-R.png");
+
+ASSET_MANAGER.downloadAll(() => {
+	const canvas = document.getElementById("gameWorld");
+	const ctx = canvas.getContext("2d");
+	ctx.imageSmoothingEnabled=false;
+
+	gameEngine.addEntity(new Alien(gameEngine));
+
+	gameEngine.init(ctx);
+
+	gameEngine.start();
+});
