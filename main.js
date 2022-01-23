6c919a79a5092c81199dc9dfd3b36f46d9a83237
Dino Jazvin <dinojaz@uw.edu>
2022-01-22 18:41:14 -0800

    pushing ant content

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-class main{}
+const gameEngine = new GameEngine();
+
+const ASSET_MANAGER = new AssetManager();
+
+ASSET_MANAGER.queueDownload("./sonic.png");
+//ASSET_MANAGER.queueDownload("./MetalDesertBackground.png");
+
+ASSET_MANAGER.downloadAll(() => {
+	const canvas = document.getElementById("gameWorld");
+	const ctx = canvas.getContext("2d");
+	ctx.imaageSmoothingEnabled = false;
+
+	
+	gameEngine.addEntity(new sonic(gameEngine));
+	//gameEngine.addEntity(new MetalDesertBackground(gameEngine));
+	gameEngine.addEntity(new back(gameEngine));
+	
+	gameEngine.init(ctx);
+
+	gameEngine.start();
+});
