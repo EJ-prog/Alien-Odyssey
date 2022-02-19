898c62993c3905e0bbf62ed217ef1c78ceebcb42
Elisabeth <56486009+EJ-prog@users.noreply.github.com>
2022-02-18 17:37:26 -0800

    BB

--- coin.js
+++ coin.js
@@ -1,8 +1,11 @@
 class Coin {
-    constructor(game){
+    constructor(game, x, y){
+        
         this.game = game;
+        this.x = x;
+        this.y = y;
         this.animator = new Animator(ASSET_MANAGER.getAsset("./Sprites_and_Assets/coin.png"), 10, 0, 50, 65, 12, 0.062);
-        this.BB = new BoundingBox(400, 160, 50, 65);
+        this.BB = new BoundingBox(this.x, this.y, 50, 65);
         //this.x = 600;
         //this.y = 365;
         //this.speed = 70;
@@ -12,7 +15,12 @@ class Coin {
         
     };
 
+    //remember to add to all parts of environment that need a BB
+    updateBB() {
+        this.BB = new BoundingBox(this.x, this.y, PARAMS.BLOCKWIDTH, (1 + 7/16) * PARAMS.BLOCKWIDTH);
+    };
+
     draw(ctx){
-        this.animator.drawFrame(this.game.clockTick, ctx, 400, 160);
+        this.animator.drawFrame(this.game.clockTick, ctx, this.x, this.y);
     };
 }
