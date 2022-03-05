9e8cbabed828bb000d8431025a36d7356dc8ac8b
dnnyle <hoangsonle2711@gmail.com>
2022-03-04 21:01:36 -0800

    updated the lavaland collsion

--- Level3/lavaLand.js
+++ Level3/lavaLand.js
@@ -2,15 +2,28 @@ class lavaLand{
     constructor(game){
         this.game = game;
         this.lavaFloor = ASSET_MANAGER.getAsset("./Level3/lavaLand.png");
+        this.x =-100;
+        this.y=520;
+        this.width = 960;
+        this.height = 200;
+        this.speed = 160;
+        this.updateBB();
 
-
+    };
+    
+    updateBB(){ 
+        this.lastBB = this.BB;
+        this.BB = new BoundingBox(this.x + 100, this.y, this.width  , this.height);
     };
 
     update(){
+        var that = this;
+        that.updateBB();
         
     };
 
     draw(ctx){
+        ctx.strokeRect(this.BB.x, this.BB.y, this.BB.width, this.BB.height);
         ctx.drawImage(this.lavaFloor, 0,300);
        
        
